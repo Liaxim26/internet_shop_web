e9b8c52b90d8ba18daacad0fa17abeb585df15f6
Liaxim26 <66952084+Liaxim26@users.noreply.github.com>
2021-04-17 04:20:28 +0300

    added header

--- src/components/layouts/v-header.vue
+++ src/components/layouts/v-header.vue
@@ -1,5 +1,53 @@
 <template>
-  <div class='v-header'>
+<header>
+<div class="top-header">
+	<div class="d-flex flex-column flex-md-row align-items-center bg-202020">
+		<nav class="my-0 mr-md-auto font-weight-normal">
+			<a class="p-2 text-white font-weight-light" href="">О компании</a>
+			<a class="p-2 text-white font-weight-light" href="">Бренды</a>
+			<a class="p-2 text-white font-weight-light" href="">Доставка</a>
+			<a class="p-2 text-white font-weight-light" href="">Оплата</a>
+			<a class="p-2 text-white font-weight-light" href="">Контакты</a>
+		</nav>
+		<a id="login" class="mt-1 btn btn-outline-light " href="">Войти</a>		
+	</div> 
+</div>	
+<div class="mid-header d-flex flex-column flex-md-row align-items-center">
+  <div class="search">
+    <div class="search-container">
+      <form method="post" action="">
+        <input type="text" placeholder="Поиск" name="search">
+      </form>
+    </div>
+  </div>
+  <div class="address v-shadow">
+      <a class="p-2 text-dark" href="">Адреса магазинов</a>
+  </div>
+  <div class="logo">
+    <a href=""><img src="../../assets/logo.jpg" alt="CloudsWorld"></a>
+  </div>
+  <div class="basket v-shadow">
+    <a class="p-2 text-dark" href="">Корзина</a>
+  </div>
+  <div class="number">
+    <h4>+7 (495) 485-15-51</h4>
+    <p class="info">Интернет-магазин работает <br>с 07:00 до 23:00 без выходных</p>
+  </div>
+</div>
+<div class="menu">
+  <nav>
+    <a class="text-dark font-weight-bold" href="">Скидки</a>
+    <a class="text-dark font-weight-bold" href="">Новинки</a>
+    <a class="text-dark font-weight-bold" href="">POD-системы</a>
+    <a class="text-dark font-weight-bold" href="">Жидкости</a>
+    <a class="text-dark font-weight-bold" href="">Вейпы</a>
+    <a class="text-dark font-weight-bold" href="">Атомайзеры</a>
+    <a class="text-dark font-weight-bold" href="">Аксессуары</a>
+  </nav>
+</div>
+</header>
+
+ <!-- <div class='v-header'>
     <router-link :to="{name: 'mainPage'}">
       <img src="../../assets/logo.png" alt="">
     </router-link>
@@ -15,7 +63,7 @@
         <i class="material-icons" @click="clearSearchField">cancel</i>
       </button>
     </div>
-    </div> 
+    </div> -->
 </template>
 
 <script>
@@ -56,7 +104,58 @@
 </script>
 
 <style lang="scss">
-  .v-header {
+  .top-header{
+    height: 50px;
+    background-color: #202020;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .mid-header{
+    height: 100px;
+    box-shadow: 0 8px 3px -9px rgba(0,0,0,0.5);
+    padding-bottom: 5px;
+    padding-top: 5px;
+  }
+  .mid-header > div {
+      /*margin-right: 3%;
+      padding-top: 5px;*/
+      float: left;
+      width: 11%;
+      padding: 10px;
+      text-align: center;
+      margin: 7.5px 3% 7.5px 5%;
+  }
+  .search{
+    padding-top: 30px;
+    padding-left: 20px;
+  }
+  .info{
+    font-size: 11px;
+  }
+  .v-shadow{
+    box-shadow: 0 8px 11px -9px rgba(0,0,0,0.2);
+    padding-bottom: 5px;
+  }
+  /*.address{
+    box-shadow: 0 8px 11px -9px rgba(0,0,0,0.2);
+    padding-bottom: 5px;
+  }
+  .basket{
+    box-shadow: 0 8px 11px -9px rgba(0,0,0,0.2);
+    padding-bottom: 5px;	
+  }*/
+  section{
+    padding-left: 33.5px;
+    padding-right: 33.5px;
+  }
+  .menu > nav >a{	
+    float: left;
+    width: 14.27%;
+    padding: 10px;
+    text-align: center;
+    margin: 7.5px 0 7.5px 0;
+  }
+ /* .v-header {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -85,5 +184,5 @@
       background: transparent;
       border: none;
     }
-  }
+  }*/
 </style>
